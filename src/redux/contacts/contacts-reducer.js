ba128929ebdba29ba72e18489d1878d59e2cfa1e
DeadNord <eosipopo@gmail.com>
2022-01-26 11:14:07 +0200

    clear comment

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -2,30 +2,6 @@ import { combineReducers } from 'redux';
 
 import { createReducer } from '@reduxjs/toolkit';
 import actions from './contacts-actions';
-// const itemsReducer = (state = [], { type, payload }) => {
-//   switch (type) {
-//     case types.ADD:
-//       return [...state, payload];
-
-//     case types.DELETE:
-//       return  ;
-
-//     default:
-//       return state;
-//   }
-// };
-// const filterReducer = (state = '', { type, payload }) => {
-//   return state;
-// };
-
-// export default combineReducers({
-//   items: itemsReducer,
-//   filter: filterReducer,
-// });
-
-// import { combineReducers } from 'redux';
-// import { createReducer } from '@reduxjs/toolkit';
-// import { addContacts, addFilter, dellContacts } from './phonebook-actions';
 
 const itemsReducer = createReducer([], {
   [actions.addContact]: (state, { payload }) => [...state, payload],
